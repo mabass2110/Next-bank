b1e8172c5fcaed5ffd2c559247b420b95e0cd8f5
helder mabasso <hfmabasso@gmail.com>
2024-09-30 20:59:40 +0200

    signup removed from navMenuItems

---
+++ config/site.ts
@@ -0,0 +1,22 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Deposit",
+      href: "/deposit",
+    },
+    {
+      label: "Withdraw",
+      href: "/withdraw",
+    },
+ 
+  ],
+};
