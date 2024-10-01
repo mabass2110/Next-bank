7d6d2af2c65d6954e8a4d9eebee0b28f2e5bebac
helder mabasso <hfmabasso@gmail.com>
2024-10-01 12:07:38 +0200

    Sign in API

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const prisma = new PrismaClient();
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
 
   if (!email || !password) {
@@ -15,11 +18,31 @@ export async function POST(request: Request) {
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (user && await bcrypt.compare(password, user.password)) {
-      return NextResponse.json({ message: 'Sign in successful' });
-    } else {
+      const tokenData = {
+        id: user.id,
+        email: user.email,
+      };
+
+      const token = jwt.sign(tokenData, process.env.AUTH_SECRET!, { expiresIn: '1h' });
+      const response = NextResponse.json({
+        message: 'Login successful',
+        success: true,
+      });
+
+      response.cookies.set('token', token, {
+        httpOnly: true,
+      });
+
+      return response;
+
+    } 
+    else {
       return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 });
     }
-  } catch (error) {
+
+  } 
+    catch (error) {
+    console.error('Internal server error:', error); // Log the error for debugging
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 }
