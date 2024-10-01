b3ef23858c558a03c6ce6b21223a535c3a20a863
helder mabasso <hfmabasso@gmail.com>
2024-10-01 13:13:28 +0200

    logout route

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -1,20 +1,30 @@
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    try{
+export async function GET() {
+    try {
         const response = NextResponse.json({
             message: "Logout Successful",
             success: true,
-        })
+        });
+
         response.cookies.set("token", "", {
-            httpOnly: true
-        })
-        return response
-    }
-    catch (error: any){
+            httpOnly: true,
+        });
+
+        return response;
+    } catch (error: unknown) {  // Change any to unknown
+        if (error instanceof Error) {
+            // Use error.message safely
+            return NextResponse.json(
+                { error: error.message },
+                { status: 500 }
+            );
+        }
+
+        // In case the error is not an instance of Error, return a generic message
         return NextResponse.json(
-            {error: error.message},
-            {status: 500}
-    )
+            { error: "An unknown error occurred." },
+            { status: 500 }
+        );
     }
 }
