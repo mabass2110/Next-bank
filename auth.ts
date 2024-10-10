83fd52fec4ffc0d0c2be13ee5f77431bfce2228d
helder mabasso <hfmabasso@gmail.com>
2024-10-10 14:23:13 +0200

    async callback to redirect after google auth

--- auth.ts
+++ auth.ts
@@ -5,5 +5,13 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   trustHost: true,
-  secret: process.env.AUTH_SECRET
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) {
+        return `${baseUrl}/deposit`; 
+      }
+      return url; 
+    },
+  }
 })
