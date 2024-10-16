6bf1c879c48bb0e15448d8f5fd264612a1b5734e
helder mabasso <hfmabasso@gmail.com>
2024-10-16 22:33:23 +0200

    OAuth

--- auth.ts
+++ auth.ts
@@ -4,12 +4,5 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      if (url.startsWith(baseUrl)) {
-        return `${baseUrl}/deposit`; 
-      }
-      return url; 
-    },
-  }
+
 })
